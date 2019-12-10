209df2b5e0785de23eb50457d67e7f7937494b27
ZorD007 <zordlegand@gmail.com>
2019-12-10 10:51:09 -0300

    wena cabros aqui va el component curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componets/login/login.component';
+import { CursoComponent } from './componets/curso/curso.component';
 
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'curso', component: CursoComponent },
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CursoComponent
   ],
   imports: [
     RouterModule.forRoot(
