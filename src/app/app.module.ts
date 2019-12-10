3cf866f4ea52f100ace97df25b0138e8358fb91e
Evelyn Correa <evelyn._.correa@hotmail.com>
2019-12-10 11:26:34 -0300

    error arreglado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,57 +6,32 @@ import { LoginComponent } from './componets/login/login.component';
 import { CursoComponent } from './componets/curso/curso.component';
 
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { HomeComponent } from './componets/home/home.component';
-
-const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
-  {path: 'home', component: HomeComponent }
-=======
-<<<<<<< HEAD
 import { AsignaturasComponent } from './componets/asignaturas/asignaturas.component';
-
-const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'asignaturas', component: AsignaturasComponent }
-=======
 import { NotasComponent } from './componets/notas/notas.component';
 
+
+
+
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-<<<<<<< HEAD
+  {path: 'home', component: HomeComponent },
+  { path: 'asignaturas', component: AsignaturasComponent},
   { path: 'curso', component: CursoComponent },
-  { path: '**', redirectTo: 'login' }
-=======
+  { path: '**', redirectTo: 'login' },
   { path: 'notas', component: NotasComponent }
->>>>>>> 6ae4aa385e2cc55aab824a395c633888110a78ca
-<<<<<<< HEAD
-=======
->>>>>>> 6f96d60ea798302857b1970536bb79ef6d441ab3
->>>>>>> 1da95bae033b695931d5438f089a84358fd8cdf9
-];
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    CursoComponent
-=======
-    NotasComponent
->>>>>>> 6ae4aa385e2cc55aab824a395c633888110a78ca
-=======
-    HomeComponent
-=======
-<<<<<<< HEAD
+    CursoComponent,
+    NotasComponent,
+    HomeComponent,
     AsignaturasComponent
-=======
-    NotasComponent
->>>>>>> 6ae4aa385e2cc55aab824a395c633888110a78ca
->>>>>>> 6f96d60ea798302857b1970536bb79ef6d441ab3
->>>>>>> 1da95bae033b695931d5438f089a84358fd8cdf9
   ],
+
   imports: [
     RouterModule.forRoot(
       appRoutes,
