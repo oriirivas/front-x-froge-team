508a014a2497020c4b312c4b3b5143d9a3fddd8c
oriana rivas <orianarivas7@gmail.com>
2019-12-10 11:07:46 -0300

    hola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componets/login/login.component';
 
-import { RouterModule, Routes } from '@angular/router';
 
+import { RouterModule, Routes } from '@angular/router';
+import { TableComponent } from './componets/table/table.component';
+
+//y todo esto tambien para la ruta
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'table', component: TableComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TableComponent
   ],
   imports: [
+    //de aquii para la ruta
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     ),
+    //aqui
     BrowserModule
   ],
   providers: [],
