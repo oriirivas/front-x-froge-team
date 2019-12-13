d910765ae14ac8e77bd35955a101c4208496b07a
angiePerez <angiehermenegildo75@gmail.com>
2019-12-13 11:38:04 -0300

    editando

--- src/app/componets/notas/notas.component.ts
+++ src/app/componets/notas/notas.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notas.component.css']
 })
 export class NotasComponent implements OnInit {
+  
 
   constructor() { }
 
   ngOnInit() {
   }
+  
+  
+  }
 
-}
