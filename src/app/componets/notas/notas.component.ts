0f2b7677c15981044b29790c5fc251acf8921508
oriana rivas <orianarivas7@gmail.com>
2019-12-13 13:15:21 -0300

    ori

--- src/app/componets/notas/notas.component.ts
+++ src/app/componets/notas/notas.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { NotasServiceService } from '../services/notas-service.service';
+
 @Component({
   selector: 'app-notas',
   templateUrl: './notas.component.html',
   styleUrls: ['./notas.component.css']
 })
 export class NotasComponent implements OnInit {
+  alumnos: string
+  asignatura: string
 
-  constructor() { }
+  constructor(private router  : Router,
+    private notaService: NotasServiceService) { }
 
   ngOnInit() {
   }
 
+  emailKeyup(value: string) {
+    this.alumnos = value;
+  }
+  passKeyup(value: string) {
+      this.asignatura = value;
+  }
+
+  getNotas(){
+    let notas =this.notaService.getNota(this.alumnos,this.asignatura);
+    let laNotas = null;
+    if(notas != null){
+        laNotas= notas;
+    }else {  
+        alert ('error');
+    }
+    return laNotas
+  }
+
 }
+
+
+  
